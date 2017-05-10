429816a6feedafc6ef9794009b3e9f376b783f3b
Joe Kitaj <joekitaj@gmail.com>
2017-05-10 14:33:24 -0400

    Dropdown works on first element

--- js/index.js
+++ js/index.js
@@ -1,28 +1,24 @@
 window.onload = () => {
-  const dropdownButtons = document.querySelectorAll('.circle-speak');
+  const dropdownButton = document.querySelector('.circle-speak');
+  const dropdown = document.querySelector('.expanded');
   const arrow = document.querySelector('.circle-speak i');
   const links = document.querySelector('.speaker-links');
+  click = 0;
 
-  for (let i = 0; i < dropdownButtons.length; i++) {
-    let click = 0;
-    if (document.addEventListener) {
-      dropdownButtons[i].addEventListener("click", function() {
-        const dropdown = this.querySelector('.expanded');
-        click += 1;
-        if((click%2) !== 0) {
-          dropdown.classList.add('show');
-          dropdown.classList.remove('hide');
-          links.style.marginTop= '100px';
-          arrow.classList.add('up-arrow');
-          arrow.classList.remove('down-arrow');
-        } else {
-          dropdown.classList.remove('show');
-          dropdown.classList.add('hide');
-          links.style.marginTop = '0';
-          arrow.classList.remove('up-arrow')
-          arrow.classList.add('down-arrow')
-        };
-      });
-    }
-  };
-};
+  const handleClick = dropdownButton.addEventListener("click",() => {
+    click += 1;
+      if((click%2) !== 0) {
+        dropdown.classList.add('show');
+        dropdown.classList.remove('hide');
+        links.style.marginTop= '100px';
+        arrow.classList.add('up-arrow');
+        arrow.classList.remove('down-arrow');
+      } else {
+        dropdown.classList.remove('show');
+        dropdown.classList.add('hide');
+        links.style.marginTop = '0';
+        arrow.classList.remove('up-arrow')
+        arrow.classList.add('down-arrow')
+      };
+    });
+}
