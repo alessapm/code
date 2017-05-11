6ec61d31c5c07e08a2dce84a1c3a5015416dc8d7
Tim Frankie <tfrankie88@gmail.com>
2017-05-11 18:27:39 -0400

    iframe video and banner

---
+++ sections/section1/js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  let controlsWrapper = document.querySelector('#player');
+  // controlsWrapper.style.visibility = "hidden";
+
+  console.log('controls wrapper children: ', controlsWrapper.children)
+});
