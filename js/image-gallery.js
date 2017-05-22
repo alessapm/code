ade0c2b89737dac21a6245f4236b26a731043db8
Marcel Hamel <marcel.hamel@gmail.com>
2017-05-22 15:46:37 -0400

    ADD removed hover effects for mobile screens in image gallery

--- js/image-gallery.js
+++ js/image-gallery.js
@@ -2,20 +2,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
   var gallerySlide = ["color1", "color2", "color3", "color4", "color5", "color6", "color7"]
 
-  for (let i=0; i< gallerySlide.length; i++) {
+  if (document.documentElement.clientWidth > 800 && document.documentElement.clientHeight > 600) {
+    for (let i=0; i< gallerySlide.length; i++) {
+      let image = document.getElementById(gallerySlide[i]);
 
-    let image = document.getElementById(gallerySlide[i]);
+      image.addEventListener("mouseover", function() {
+        let newNum = i + 1;
+        image.src = './images/section7_images/gallery' + newNum + '.jpg';
+      })
 
-    image.addEventListener("mouseover", function() {
+      image.addEventListener("mouseout", function() {
+        let newNum = i + 1;
+        image.src = './images/section7_images/gallery' + newNum + '-blue.jpg';
+      })
+    }
+
+  } else {
+    for (let i=0; i< gallerySlide.length; i++) {
+      let image = document.getElementById(gallerySlide[i]);
       let newNum = i + 1;
-      image.src = `./images/section7_images/gallery${newNum}.jpg`;
-    })
+      image.setAttribute('src', './images/section7_images/gallery' + newNum + '.jpg');
 
-    image.addEventListener("mouseout", function() {
-      let newNum = i + 1;
-      image.src = `./images/section7_images/gallery${newNum}-blue.jpg`;
-    })
-
+    }
   }
 
 });
