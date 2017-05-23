c490f4e177f025266b6ea4c319ab3abf1e5a70c8
Marcel Hamel <marcel.hamel@gmail.com>
2017-05-23 08:59:28 -0400

    Speaker Carousel Responsiveness

--- js/section4.js
+++ js/section4.js
@@ -8,57 +8,48 @@ document.addEventListener('DOMContentLoaded', () => {
   const carouselButtons = document.querySelectorAll('.carousel-button')
 
   // More info functionality
-  for (let y = 0; y < dropdownButtons.length; y++) {
+  for (let i = 0; i < dropdownButtons.length; i++) {
     let click = 0;
-    dropdownButtons[y].addEventListener('click', () => {
+    dropdownButtons[i].addEventListener('click', () => {
       click += 1;
       if((click%2) !== 0) {
-        dropdowns[y].classList.add('show');
-        dropdowns[y].classList.remove('hide');
+        dropdowns[i].classList.add('show');
+        dropdowns[i].classList.remove('hide');
         links.style.marginTop= '100px';
-        arrows[y].classList.add('up-arrow');
-        arrows[y].classList.remove('down-arrow');
+        arrows[i].classList.add('up-arrow');
+        arrows[i].classList.remove('down-arrow');
       } else {
           click = 0;
-          dropdowns[y].classList.remove('show');
-          dropdowns[y].classList.add('hide');
+          dropdowns[i].classList.remove('show');
+          dropdowns[i].classList.add('hide');
           links.style.marginTop = '0';
-          arrows[y].classList.remove('up-arrow')
-          arrows[y].classList.add('down-arrow')
+          arrows[i].classList.remove('up-arrow')
+          arrows[i].classList.add('down-arrow')
       };
     });
   }
 
-  // Old Carousel functionality
+  // Carousel functionality
 
-  var currentSpeakers = [];
-  var numberOfSpeakerBoxes;
+  let currentSpeakers = [];
 
-  if (document.documentElement.clientWidth > 950) {
-    numberOfSpeakerBoxes = 3;
-  } else if (document.documentElement.clientWidth > 700) {
-    numberOfSpeakerBoxes = 2;
-  } else {
-    numberOfSpeakerBoxes = 1;
-  }
-
-  for (x = 0; x < speakers.length; x++) {
+  for (let x = 0; x < speakers.length; x++) {
     currentSpeakers.push(speakers[x])
     if (x >= 3) {
       currentSpeakers[x].classList.add('hide')
     }
   }
 
-  for (i = 0; i < carouselButtons.length; i++) {
+  for (let i = 0; i < carouselButtons.length; i++) {
     carouselButtons[i].addEventListener('click', () => {
-      for (j = 0; j < currentSpeakers.length; j++) {
+      for (let j = 0; j < currentSpeakers.length; j++) {
         if (j < 3) {
           currentSpeakers[j].classList.remove('hide')
         } else {
           currentSpeakers[j].classList.add('hide')
         }
       }
-      var droppedSpeaker = currentSpeakers.shift()
+      let droppedSpeaker = currentSpeakers.shift()
       currentSpeakers.push(droppedSpeaker);
     })
   }
